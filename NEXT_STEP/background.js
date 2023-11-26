6221c1145bbe35b03400bb737397ae85ef3b4094
Rachappa <97609401+RachappaB@users.noreply.github.com>
2023-11-26 07:09:42 +0530

    12

--- NEXT_STEP/background.js
+++ NEXT_STEP/background.js
@@ -1,20 +1,3 @@
-chrome.tabs.onCreated.addListener(function (tab) {
-    chrome.storage.local.get("photos", function (result) {
-      const photos = result.photos || [];
-      if (photos.length > 0) {
-        const randomIndex = Math.floor(Math.random() * photos.length);
-        const randomPhoto = photos[randomIndex];
-        chrome.tabs.update(tab.id, { "image": randomPhoto });
-      }
-    });
-  });
-  
-
-  chrome.runtime.onInstalled.addListener(function () {
-    const photos = [];
-    for (let i = 1; i <= 10; i++) {
-      photos.push(`photos/photo${i}.jpg`);
-    }
-    chrome.storage.local.set({ "photos": photos });
-  });
-  
+chrome.tabs.onCreated.addListener(function(tab) {
+  chrome.tabs.update(tab.id, {url: "index.html"});
+});
